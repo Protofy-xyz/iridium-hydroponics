ba02c278cf4768d9d747ce44912f3e7dab253fb4
RogerOrRobert <roger.lopez@protofy.xyz>
2024-06-25 21:20:19 +0200

    [FIX] plant page appears on compiling

--- packages/app/bundles/custom/pages/plant.tsx
+++ packages/app/bundles/custom/pages/plant.tsx
@@ -7,31 +7,30 @@ Paginated apis return an object like: {"itemsPerPage": 25, "items": [...], "tota
 
 import { Protofy } from 'protolib/base'
 import { Objects } from 'app/bundles/objects'
-import { DataView, API, AdminPage, PaginatedDataSSR } from 'protolib'
-import { Tag } from '@tamagui/lucide-icons'
+import { DataView, AdminPage, SSR, withSession } from 'protolib'
 import React from 'react'
+import { Router } from '@tamagui/lucide-icons';
+import { DevicesModel } from 'protolib/bundles/devices/devices';
 
-const Icons = {}
-const isProtected = Protofy("protected", true)
-const { actionFetch } = API;
+const isProtected = Protofy("protected", false)
 const { name, prefix } = Objects.plant.getApiOptions()
 const sourceUrl = prefix + name
 
 export default {
-    route: Protofy("route", "workspace/dev/plant"),
-    component: ({ pageState, initialItems, pageSession, extraData }: any) => {
-        return (<AdminPage title="Plant" pageSession={pageSession} integratedChat={false}>
-            <DataView
-                rowIcon={Tag}
-                sourceUrl={sourceUrl}
-                initialItems={initialItems}
-                numColumnsForm={2}
-                name="Plant"
-                model={Objects.plant}
-                pageState={pageState}
-                icons={Icons}
-            />
-        </AdminPage>)
-    },
-    getServerSideProps: PaginatedDataSSR(sourceUrl, isProtected ? Protofy("permissions", []) : undefined)
+  route: Protofy("route", "/workspace/dev/plant"),
+  component: ({ pageState, initialItems, pageSession, extraData }: any) => {
+    return (<AdminPage title="plant" pageSession={pageSession}>
+      <DataView
+        defaultView={"list"}
+        integratedChat
+        rowIcon={Router}
+        sourceUrl={sourceUrl}
+        initialItems={initialItems}
+        name="plant"
+        model={DevicesModel}
+        pageState={pageState}
+      />
+    </AdminPage>)
+  },
+  getServerSideProps: SSR(async (context) => withSession(context, isProtected ? Protofy("permissions", []) : undefined))
 }
